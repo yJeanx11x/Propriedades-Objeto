2dd4d8a59afdf6444d19c555249c19dc7b5c29c2
Jean Lucas <j3anlucas010@gmail.com>
2024-06-21 10:54:45 -0300

    Objetos

---
+++ script.js
@@ -0,0 +1,9 @@
+// OBJECT
+//object.keys(retorna as chaves)
+//object.freeze(congela o objeto)
+//object.defineProperties(define varias Propriedades)
+//object.defineProperty(Define um Propriedade)
+//object.value(retorna o Valor)
+//object.entries(retorna chaves e valores em arrays)
+//object.getOwnPropertyDescriptor(retorna um descritor de propriedades para uma propriedade )
+//object.assign(Copia um Objeto)
